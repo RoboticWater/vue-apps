76adf55a8cd2df28caf99eefa507ca8c410f1fad
John <jakebritti@gmail.com>
2022-02-18 14:27:48 -0500

    added test component

---
+++ src/apps/Test_Vue_Component/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <div id="room" class="darkwall">
+  <Title msg="Reality Media" />
+	<img src="../../assets/images/rotunda-map.png" width="250" >
+	<div class="displaytext">AR allows us to extend our physical reality; VR creates for us a different reality.</div>
+  </div>  -->
+  <div class="viewer"></div>
+</template>
+
+<script setup>
+// import Title from '../../components/CenterTitle.vue'
+
+import "../../assets/top.css";
+import "../../assets/room.css";
+</script>
+
+<style scoped>
+.viewer {
+  width: 640px;
+  height: 480px;
+  animation: intro 1s cubic-bezier(0.58, 0.09, 0.21, 0.99);
+  border: 2px solid #f00;
+}
+@keyframes intro {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
