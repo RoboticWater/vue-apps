a6eb2009e73133fbc8128cf136390b43964d5c7c
John <jakebritti@gmail.com>
2022-02-24 10:19:33 -0500

    fixes

--- src/apps/Test_Vue_Component/App.vue
+++ src/apps/Test_Vue_Component/App.vue
@@ -4,11 +4,13 @@
 	<img src="../../assets/images/rotunda-map.png" width="250" >
 	<div class="displaytext">AR allows us to extend our physical reality; VR creates for us a different reality.</div>
   </div>  -->
+  <Title msg="Test Vue Component" />
+  <div id="room" class="darkwall">
   <div class="viewer"></div>
 </template>
 
 <script setup>
-// import Title from '../../components/CenterTitle.vue'
+import Title from "../../components/CenterTitle.vue";
 
 import "../../assets/top.css";
 import "../../assets/room.css";
