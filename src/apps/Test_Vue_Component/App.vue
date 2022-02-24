2de6fa02ac59451d0ddb86e235059bb81336ddfa
John <jakebritti@gmail.com>
2022-02-24 10:48:24 -0500

    change around css

--- src/apps/Test_Vue_Component/App.vue
+++ src/apps/Test_Vue_Component/App.vue
@@ -13,21 +13,4 @@ import "../../assets/room.css";
 </script>
 
 <style scoped>
-.viewer {
-  width: 640px;
-  height: 480px;
-  animation: intro 1s cubic-bezier(0.58, 0.09, 0.21, 0.99);
-  border: 2px solid #f00;
-}
-.viewer:hover {
-  background: #ff0;
-}
-@keyframes intro {
-  0% {
-    transform: scale(0);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
 </style>
